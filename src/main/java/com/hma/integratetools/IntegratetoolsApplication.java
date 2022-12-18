2e3cf198f1631c5e0fdb76b2d132e6f5c86ed9eb
henri-marchal-accenture <henri.marchal@accenture.com>
2022-12-18 22:14:58 +0100

    first commit

---
+++ src/main/java/com/hma/integratetools/IntegratetoolsApplication.java
@@ -0,0 +1,13 @@
+package com.hma.integratetools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegratetoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegratetoolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hma/integratetools/IntegratetoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hma.integratetools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegratetoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
